75dfe74a882f224d35b2ec82424285caff862932
ntswakikekana <ntswakikekana46@gmail.com>
2024-06-18 07:10:36 -0700

    updated

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Script that queries subscribers on a given Reddit subreddit.
+"""
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """
+    Return the total number of subscribers on a given subreddit.
+    If the subreddit is invalid, return 0.
+    """
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0"}
+
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 200:
+            data = response.json()
+            return data['data']['subscribers']
+        else:
+            return 0
+    except requests.RequestException:
+        return 0
+
+
+if __name__ == "__main__":
+    subreddit = input("Enter the subreddit name: ")
+    subscribers = number_of_subscribers(subreddit)
+    print(f"The subreddit '{subreddit}' has {subscribers} subscribers.")
+
